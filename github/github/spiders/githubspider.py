3d4c59a0c33bb1ee3a14511d71ce706b51c33377
ayang818 <41252899+ayang818@users.noreply.github.com>
2018-12-19 11:09:26 +0800

    Add files via upload

---
+++ github/github/spiders/githubspider.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.http import FormRequest,Request
+
+class GithubspiderSpider(scrapy.Spider):
+    name = 'githubspider'
+    
+    
+    def start_requests(self):
+        yield Request('https://github.com/login',callback=self.log)
+    
+    def log(self,response):
+        fd={'login':'1004210191@qq.com','password':'yfc719818'}
+        yield FormRequest.from_response(response,formdata=fd,callback=self.parsepage)
+    
+    def parsepage(self,response):
+        with open('D:/Mygithub.html','wb') as f:
+            f.write(response.body)
